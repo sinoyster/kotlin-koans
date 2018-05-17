b5b8a21a89f56f283b6de2aab8cbcf480d35902f
Yuk Wong@x230 <sinoyster@gmail.com>
2018-05-17 10:38:59 +0800

    finish i_introduction

--- src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
@@ -14,5 +14,6 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
 
 
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    //todoTask1(collection)
+    return "{${collection.joinToString(", ")}}"
 }
--- src/ii_collections/n14FilterMap.kt
+++ src/ii_collections/n14FilterMap.kt
@@ -10,12 +10,19 @@ fun example1(list: List<Int>) {
 
 fun Shop.getCitiesCustomersAreFrom(): Set<City> {
     // Return the set of cities the customers are from
-    todoCollectionTask()
+    // return this.customers
+    /*
+    var cities = HashSet<City>()
+    this.customers.forEach({cities.add(it.city)})
+    return cities
+    */
+    return  this.customers.map { it.city }.toSet()
+
 }
 
 fun Shop.getCustomersFrom(city: City): List<Customer> {
     // Return a list of the customers who live in the given city
-    todoCollectionTask()
+    return this.customers.filter { it.city == city }
 }
 
 
